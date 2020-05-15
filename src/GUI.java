f43c7fbc4f8582e5dcc7eca3a15383758933ef10
danielpalle <daniel.gustafsson@live.se>
2020-05-15 15:49:34 +0200

    created project Virus-Game

---
+++ src/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GUI {
+    JFrame frame = new JFrame();
+
+    public GUI() {
+        buildGameWindow();
+    }
+
+    private void buildGameWindow(){
+
+    }
+
+}
